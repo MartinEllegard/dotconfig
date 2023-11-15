8fbdaf768a837395d991724bc13278c265998d91
Martin Ellegård <martin.ellegard@gmail.com>
2023-11-15 12:43:07 +0100

    init

---
+++ xmonad/lib/Colors/CatppuccinMocha.hs
@@ -0,0 +1,61 @@
+module Colors.CatppuccinMocha where
+
+import XMonad
+
+colorScheme = "catppuccin-mocha"
+
+--Border Colors
+colorBack = "#585b70"
+
+colorFore = "#cba6f7"
+
+color01 = "#1c1f24"
+
+--
+color02 = "#ff6c6b"
+
+--
+color03 = "#98be65"
+
+--
+color04 = "#da8548"
+
+--
+color05 = "#6c7086"
+
+-- Current tab
+color06 = "#cba6f7"
+
+--
+color07 = "#5699af"
+
+--
+color08 = "#6c7086"
+
+-- Seperator
+color09 = "#585b70"
+
+--
+color10 = "#da8548"
+
+--
+color11 = "#4db5bd"
+
+--
+color12 = "#ecbe7b"
+
+--
+color13 = "#3071db"
+
+
+--
+color14 = "#a9a1e1"
+
+-- Fucus/Active
+color15 = "#cba6f7"
+
+-- Title
+color16 = "#89b4fa"
+
+colorTrayer :: String
+colorTrayer = "--tint 0x282c34"
